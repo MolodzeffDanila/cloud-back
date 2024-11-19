b1c0b3d7dc08111827cb38e83ac68cb0b5b13a68
d.molodtsev <d.molodtsev@tinkoff.ru>
2024-11-19 21:01:41 +0300

    feat: added comm

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { LoginController } from './login.controller';
-//
+
 @Module({
   controllers: [LoginController],
   providers: [LoginService],
